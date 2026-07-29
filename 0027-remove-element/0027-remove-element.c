6741ea84cbdc3b6caf365a4cf6060360fa77653c
Suman Sett <sumansett4@gmail.com>
2026-07-29 20:31:45 +0530

    Time: 0 ms (100%), Space: 10.5 MB (29.1%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.c
@@ -0,0 +1,15 @@
+int removeElement(int* nums, int numsSize, int val) {
+    int i,j=0;
+    for(i=0;i<numsSize;i++)
+    {
+        if(nums[i]!=val)
+        {
+            nums[j]=nums[i];
+            j++;
+        }
+        else{
+            continue;
+        }
+    }
+    return j;
+}
