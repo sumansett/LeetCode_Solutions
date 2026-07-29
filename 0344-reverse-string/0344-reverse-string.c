e811eccfc4b48a258fcb879041efe3a18e39df82
Suman Sett <sumansett4@gmail.com>
2026-07-29 21:59:19 +0530

    Time: 0 ms (100%), Space: 17.9 MB (47.3%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.c
@@ -0,0 +1,15 @@
+void reverseString(char* s, int sSize) {
+    int i=0,j=sSize-1;
+    char temp;
+
+    while(i<j)
+    {
+        temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
+
+        i++;
+        j--;
+    }
+
+}
