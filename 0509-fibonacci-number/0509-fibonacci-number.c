0f358aa90f1704e89cba293c3d46d839bcfde5d3
Suman Sett <sumansett4@gmail.com>
2026-08-11 10:00:52 +0530

    Time: 3 ms (68.52%), Space: 8.5 MB (63.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.c
+++ 0509-fibonacci-number/0509-fibonacci-number.c
@@ -1,9 +1,22 @@
 
 
 int fib(int n){
+    
     if(n==0)
         return 0;
-    if(n==1)
+    if (n==1)
         return 1;
-    return fib(n-1) + fib(n-2);
+    
+    int a[n+1],i;
+
+    a[0]=0;
+    a[1]=1;
+
+    for(i=2;i<=n;i++)
+    {
+        a[i]=a[i-1]+a[i-2];
+    }
+
+    return a[n];
+
 }
