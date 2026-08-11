ade867d329ec176d1357fcba42c8352b670477a7
Suman Sett <sumansett4@gmail.com>
2026-08-11 13:11:59 +0530

    Time: 0 ms (100%), Space: 27.2 MB (66.1%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.c
+++ 0189-rotate-array/0189-rotate-array.c
@@ -13,8 +13,12 @@ void reverse(int* nums,int left,int right)
 }
 void rotate(int* nums, int numsSize, int k) {
     
-    k=k%numsSize;
-    reverse(nums,0,numsSize-1);
-    reverse(nums,0,k-1);
-    reverse(nums,k,numsSize-1);
+    if(numsSize>1)
+    {
+        k=k%numsSize;
+        reverse(nums,0,numsSize-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,numsSize-1);
+    }
+    
 }
